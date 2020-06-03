7d6a07e9f31b7c9eadf3f2aaddff645aa6f87251
Benjamin Wilkins <62174924+benjamin-wilkins@users.noreply.github.com>
2020-06-03 19:31:24 +0100

    Create script.js

---
+++ calm-color/script.js
@@ -0,0 +1,16 @@
+container = document.getElementById("container")
+container.innerhtml = "<button type="button" id="button">Change Color</button>"
+button = document.getElementById("button")
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+button.onclick = function {
+  document.body.style.background = getRandomColor();
+}
